9555edf2dede9a60510287d45f7ae14c035d12ff
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-10 13:50:05 +0700

    FintechHub-Ruby-finnhub-mangopay-hook/rb

---
+++ lib/mangopay/hook.rb
@@ -0,0 +1,10 @@
+module MangoPay
+
+      # See http://docs.mangopay.com/api-references/notifications/
+      class Hook < Resource
+        include HTTPCalls::Create
+        include HTTPCalls::Update
+        include HTTPCalls::Fetch
+      end
+    end
+    
